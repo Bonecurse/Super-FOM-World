52b9f368d31f2ed2d494efdb8e2af2c86062203f
vipersiegen <daniel@neumann-siegen.de>
2025-05-11 17:30:49 +0200

    Test-Push

--- scripts/player.gd
+++ scripts/player.gd
@@ -14,7 +14,7 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	# Projekt -> Projekteinstellungen -> Eingabezuordnung um eigene Actions zu erstellen und Key Bindings
-	if Input.is_action_just_pressed("jump"): #and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction: -1, 0, 1

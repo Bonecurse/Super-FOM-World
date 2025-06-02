698389e2996ef2cb8b6d95b90c5957facf5bf4d3
vipersiegen <daniel@neumann-siegen.de>
2025-06-02 10:27:10 +0200

    Block, die von unten angesprungen werden können und einen Coin freigeben implementiert

--- scenes/FoMcoin.gd
+++ scenes/FoMcoin.gd
@@ -1,12 +1,9 @@
 extends Area2D
-@onready var game_manager: Node = %GameManager
-
- 
-
+@onready var hud = get_tree().root.get_node("level_2/HUD")
 
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
 #		game_manager.add_point()
 		queue_free()
-		%HUD.add_punkte(100)
+		hud.add_punkte(100)
 		
---
+++ scripts/CoinBlock.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+@export var coin_scene: PackedScene = preload("res://scenes/FOMcoin.tscn")
+@onready var spawn_point = $SpawnPoint
+@onready var sprite = $Coinbox
+
+var used = false
+
+func hit_from_below():
+	if used:
+		return
+	used = true
+	spawn_coin()
+	sprite.texture = preload("res://assets/sprites/usedblock.png")  # falls du eine "leere" Version hast
+	# Optional: Animation abspielen, wenn vorhanden
+	if has_node("AnimationPlayer"):
+		$AnimationPlayer.play("used")
+
+func spawn_coin():
+	var coin = coin_scene.instantiate()
+#	get_tree().current_scene.add_child(coin)
+#	coin.global_position = spawn_point.global_position
+	coin.global_position = spawn_point.global_position
+	get_tree().current_scene.call_deferred("add_child", coin)
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	hit_from_below()

ee2ae6ed6d822d684af34a2b2e65dc0ee4929c6c
Schupas <github@schupas.de>
2025-07-11 21:58:06 +0200

    Sounds angepasst

--- scenes/FoMcoin.gd
+++ scenes/FoMcoin.gd
@@ -1,11 +1,5 @@
 extends Area2D
 @onready var hud = get_tree().root.get_node("level/HUD")
-@export var pop_height := 16      # Pixel nach oben
-@export var pop_time   := 1    # Sek. für den Sprung
-@export var auto_remove := true   # am Ende löschen?
-@export var pop_offset := 16      # Pixel, um die der Startpunkt nach unten versetzt wird.
-
-
 
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
@@ -19,16 +13,3 @@ func _on_body_entered(body):
 		queue_free()
 		hud.add_punkte(100)
 		
-func pop_up():
-	# Safety: Methode nur starten, wenn Coin schon im Scene-Tree hängt
-	if not is_inside_tree():
-		await ready
-	
-	position.y += pop_offset
-
-	var tween := create_tween()
-	tween.tween_property(
-		self, "position:y",
-		position.y- pop_height,
-		pop_time
-	).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)

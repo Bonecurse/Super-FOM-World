559c3a3db3090865b349a0c7d9b82fe83f45ea9e
Schupas <github@schupas.de>
2025-07-12 00:19:20 +0200

    Sounds, Hintergründe, Volume, Charakter

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -25,3 +25,16 @@ func showcoins():
 
 func update_levelcoins(ist,von):
 	%levelcoins.text = "LEVELCOINS: " + str(ist) + " / " + str(von)
+
+func _on_volume_slider_value_changed(value: float) -> void:
+	var bus_index = AudioServer.get_bus_index("Master")
+	AudioServer.set_bus_volume_db(bus_index, value)
+	ProjectSettings.set_setting("user/volume", value)
+	ProjectSettings.save()
+	
+func _ready() -> void:
+	var saved_volume = ProjectSettings.get_setting("user/volume", 0)
+	if %VolumeSlider:
+		%VolumeSlider.value = saved_volume
+	var bus_index = AudioServer.get_bus_index("Master")
+	AudioServer.set_bus_volume_db(bus_index, saved_volume)
